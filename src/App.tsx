6d915e4a864bc428d50a51a1d8521e4f769e2f12
pasan-nadeeja-dev <pasannadeeja94@gmail.com>
2021-05-04 22:18:01 +0530

    Refactor cra template

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,14 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import { useQuery } from "react-query";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+import Drawer from "@material-ui/core/Drawer";
+import LinearProgress from "@material-ui/core/LinearProgress";
+import Grid from "@material-ui/core/Grid";
+import Badge from "@material-ui/core/Badge";
+import AddShoppingCart from "@material-ui/icons/AddShoppingCart";
+
+const App = () => {
+  return <div className="App">start</div>;
+};
 
 export default App;
